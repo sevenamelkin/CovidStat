c462aef0b8eea9010c9d52356bea6369a6d8ffa8
amelkin-sa <sevenamelkin@gmail.com>
2021-07-11 23:01:10 +0300

    Init commit

---
+++ CovidStat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace CovidStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToInt("176.212.181.60"));
+        }
+        
+        static long ToInt(string addr)
+        {
+            // careful of sign extension: convert to uint first;
+            // unsigned NetworkToHostOrder ought to be provided.
+            return (long) (uint) IPAddress.NetworkToHostOrder(
+                (int) IPAddress.Parse(addr).Address);
+        }
+    }
+}
